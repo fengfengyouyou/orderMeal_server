f0f6dba91adb1f0b47d6838a77340a8376732b0f
vector vector <fengfengyouyou@gmail.com>
2020-04-01 10:31:41 +0800

    修改首页

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '这里没有东西哦！' });
 });
 
 module.exports = router;
